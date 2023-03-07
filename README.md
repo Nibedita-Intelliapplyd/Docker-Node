247cab9e5ac957ec9cd44add8717bcfb253f8ba7
Nibedita <nibedita@intelliapplyd.com>
2023-03-07 12:40:23 +0530

    new files added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Docker-Node
+Docker based app for node
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3600;
+
+app.get("/", (req, res)=>{
+    console.log("Req hits")
+    res.send("Hello DOCKER COMPOSE prod");
+})
+
+app.listen(port, ()=>{
+    console.log(`Docker Node app on listening on port ${port}`)
+})
